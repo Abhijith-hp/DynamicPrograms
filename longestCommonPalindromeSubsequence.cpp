327147bdc91c78d18f2bf203b5ecd076bb083406
ABHIJITH G <20309@gtl.loc>
2025-10-02 16:48:55 +0530

    Implemented the longest common palindrome  subsequence using tabulation

--- longestCommonPalindromeSubsequence.cpp
+++ longestCommonPalindromeSubsequence.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int helper(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
-       if(i<0 || j<0) return 0;
-       if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j])
-           dp[i][j]=1+helper(i-1,j-1,s1,s2,dp);
-        else
-            dp[i][j]=max(helper(i-1,j,s1,s2,dp),helper(i,j-1,s1,s2,dp));
-        return dp[i][j];
-
-    }
     int longestPalindromeSubseq(string s) {
         int length = s.size();
         string s1 = s;
         reverse(s.begin(),s.end());
-        vector<vector<int>>dp(length,vector<int>(length,-1));
-        return helper(length-1,length-1,s1,s,dp);
+        vector<vector<int>>dp(length+1,vector<int>(length+1,0));
+
+        for(int i=1;i<=length;i++){
+            for(int j=1;j<=length;j++){
+                if(s1[i-1]==s[j-1])
+                    dp[i][j] = 1+ dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[length][length];
+        
     }
 };
