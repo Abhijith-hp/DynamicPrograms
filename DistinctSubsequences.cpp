384fb2d6db0c12a068911c7ce0dcf5b55657d285
ABHIJITH G <20309@gtl.loc>
2025-10-02 21:32:13 +0530

    Implemented a solution to find distinct subsequences using tabulation

--- DistinctSubsequences.cpp
+++ DistinctSubsequences.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int helper(int i,int j,string &s,string &t,vector<vector<int>>&dp){
-
-        if(j<0) return 1;
-        if(i<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j])
-          dp[i][j]= helper(i-1,j-1,s,t,dp)+helper(i-1,j,s,t,dp);
-        else
-          dp[i][j]= helper(i-1,j,s,t,dp);
-        
-        return dp[i][j];
-        
-    }
+  
     int numDistinct(string s, string t) {
-        vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
-        return helper(s.size()-1,t.size()-1,s,t,dp);
+        int m = s.size();
+        int n = t.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=0;i<=m;i++) dp[i][0]=1;
+       
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==t[j-1])
+                  dp[i][j]= dp[i-1][j-1]+dp[i-1][j];
+                else
+                  dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[m][n];
     }
 };
