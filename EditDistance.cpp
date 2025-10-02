f91114f60a95663ff83bec8e30021d0bac2dd983
ABHIJITH G <20309@gtl.loc>
2025-10-03 00:12:34 +0530

    Implemented a solution for the problem of edit distance using tabulation

--- EditDistance.cpp
+++ EditDistance.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int helper(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j])
-           dp[i][j]=helper(i-1,j-1,s1,s2,dp);
-        else
-          dp[i][j]=min(min(1+helper(i,j-1,s1,s2,dp),1+helper(i-1,j,s1,s2,dp)),1+helper(i-1,j-1,s1,s2,dp));
-        return dp[i][j];
-         
-    }
+
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),-1));
-        return helper(word1.size()-1,word2.size()-1,word1,word2,dp);
+        int m= word1.size();
+        int n = word2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        for(int i=0;i<=m;i++) dp[i][0]=i;
+        for(int j=1;j<=n;j++) dp[0][j]=j;
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1]==word2[j-1])
+                  dp[i][j] = dp[i-1][j-1];
+                else
+                  dp[i][j]=min(min(1+dp[i][j-1],1+dp[i-1][j]),1+dp[i-1][j-1]);
+                     
+            }
+        }
+        return dp[m][n];
     }
 };
