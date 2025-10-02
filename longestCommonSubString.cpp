d06b6410fd9c8da0d2b318356a18c6d864ebe303
ABHIJITH G <20309@gtl.loc>
2025-10-02 14:04:58 +0530

    Implemented the longest common subsequence using memosiation

--- longestCommonSubString.cpp
+++ longestCommonSubString.cpp
@@ -1,21 +1,24 @@
 class Solution {
   public:
     int ans =0;
-    int lengthOfSubStr(int i,int j,string& s1,string& s2){
+    int lengthOfSubStr(int i,int j,string& s1,string& s2,vector<vector<int>>&dp){
         int curr = 0;
         if(i<0 || j<0) return 0;
+        if (dp[i][j]!=-1) return dp[i][j];
         if(s1[i]==s2[j]){
-            curr =  1+lengthOfSubStr(i-1,j-1,s1,s2);
-            ans =max(ans,curr);
+            dp[i][j] =   1+lengthOfSubStr(i-1,j-1,s1,s2,dp);
+            ans =max(ans,dp[i][j]);
             
-        }
-        lengthOfSubStr(i-1,j,s1,s2);
-        lengthOfSubStr(i,j-1,s1,s2);
-        return curr;
+        }else
+          dp[i][j]=0;
+        lengthOfSubStr(i-1,j,s1,s2,dp);
+        lengthOfSubStr(i,j-1,s1,s2,dp);
+        return dp[i][j];
     
     }
     int longestCommonSubstr(string& s1, string& s2) {
-        lengthOfSubStr(s1.size()-1,s2.size()-1,s1,s2);
+        vector<vector<int>>dp(s1.size(),vector<int>(s2.size(),-1));
+        lengthOfSubStr(s1.size()-1,s2.size()-1,s1,s2,dp);
         return ans;
         
     }
