6703da189dd7d0b770254c9996fc9ef7f49165f9
ABHIJITH G <20309@gtl.loc>
2025-10-02 16:48:24 +0530

    Implemented the longest common palindrome  subsequence using memoziation

--- longestCommonPalindromeSubsequence.cpp
+++ longestCommonPalindromeSubsequence.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int helper(int i,int j,string &s1,string &s2){
+    int helper(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
        if(i<0 || j<0) return 0;
-
+       if(dp[i][j]!=-1) return dp[i][j];
         if(s1[i]==s2[j])
-           return 1+helper(i-1,j-1,s1,s2);
-        return max(helper(i-1,j,s1,s2),helper(i,j-1,s1,s2));
+           dp[i][j]=1+helper(i-1,j-1,s1,s2,dp);
+        else
+            dp[i][j]=max(helper(i-1,j,s1,s2,dp),helper(i,j-1,s1,s2,dp));
+        return dp[i][j];
 
     }
     int longestPalindromeSubseq(string s) {
         int length = s.size();
         string s1 = s;
         reverse(s.begin(),s.end());
-        return helper(length-1,length-1,s1,s);
+        vector<vector<int>>dp(length,vector<int>(length,-1));
+        return helper(length-1,length-1,s1,s,dp);
     }
 };
