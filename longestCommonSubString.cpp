4bf543636335dda2eb3049631d02ce4563e00e6b
ABHIJITH G <20309@gtl.loc>
2025-10-02 14:05:30 +0530

    Implemented the longest common subsequence using tabulation

--- longestCommonSubString.cpp
+++ longestCommonSubString.cpp
@@ -1,24 +1,21 @@
 class Solution {
   public:
-    int ans =0;
-    int lengthOfSubStr(int i,int j,string& s1,string& s2,vector<vector<int>>&dp){
-        int curr = 0;
-        if(i<0 || j<0) return 0;
-        if (dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j]){
-            dp[i][j] =   1+lengthOfSubStr(i-1,j-1,s1,s2,dp);
-            ans =max(ans,dp[i][j]);
-            
-        }else
-          dp[i][j]=0;
-        lengthOfSubStr(i-1,j,s1,s2,dp);
-        lengthOfSubStr(i,j-1,s1,s2,dp);
-        return dp[i][j];
     
-    }
     int longestCommonSubstr(string& s1, string& s2) {
-        vector<vector<int>>dp(s1.size(),vector<int>(s2.size(),-1));
-        lengthOfSubStr(s1.size()-1,s2.size()-1,s1,s2,dp);
+        vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,0));
+        //lengthOfSubStr(s1.size()-1,s2.size()-1,s1,s2,dp);
+        int ans = 0;
+        for(int i =1;i<=s1.size();i++ ){
+            for(int j=1;j<=s2.size();j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                    ans = max(ans,dp[i][j]);
+                    
+                }else
+                   dp[i][j]=0;
+                
+            }
+        }
         return ans;
         
     }
