d56ff63223a49f31da7c4a6962af194b7a1459b6
ABHIJITH G <20309@gtl.loc>
2025-10-03 00:13:19 +0530

    Implemented a solution for the problem of edit distance using tabulation by optimizing into single array

--- EditDistance.cpp
+++ EditDistance.cpp
@@ -4,19 +4,21 @@ public:
     int minDistance(string word1, string word2) {
         int m= word1.size();
         int n = word2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        for(int i=0;i<=m;i++) dp[i][0]=i;
-        for(int j=1;j<=n;j++) dp[0][j]=j;
+        vector<int>prev(n+1,0),curr(n+1,0);
+       
+        for(int j=1;j<=n;j++) prev[j]=j;
         
         for(int i=1;i<=m;i++){
+            curr[0]=i;
             for(int j=1;j<=n;j++){
                 if(word1[i-1]==word2[j-1])
-                  dp[i][j] = dp[i-1][j-1];
+                  curr[j] = prev[j-1];
                 else
-                  dp[i][j]=min(min(1+dp[i][j-1],1+dp[i-1][j]),1+dp[i-1][j-1]);
+                  curr[j]=min(min(1+curr[j-1],1+prev[j]),1+prev[j-1]);
                      
             }
+            prev=curr;
         }
-        return dp[m][n];
+        return prev[n];
     }
 };
