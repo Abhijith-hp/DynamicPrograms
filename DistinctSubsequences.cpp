66103c9616fbfb9d49995f1fd88692c6da5131f2
ABHIJITH G <20309@gtl.loc>
2025-10-02 21:33:11 +0530

    Implemented a solution to find distinct subsequences using tabulation by space optimising in singl array

--- DistinctSubsequences.cpp
+++ DistinctSubsequences.cpp
@@ -3,19 +3,15 @@ public:
     int numDistinct(string s, string t) {
         int m = s.size();
         int n = t.size();
-        vector<double>prev(n+1,0),curr(n+1,0);
-        curr[0]=prev[0]=1;
-       
-
+        vector<double>dp(n+1,0);
+        dp[0]=1;   
         for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
+            for(int j=n;j>=1;j--){
                 if(s[i-1]==t[j-1])
-                  curr[j]= prev[j-1]+prev[j];
-                else
-                  curr[j] = prev[j];
+                  dp[j]= dp[j-1]+dp[j];
+                
             }
-            prev=curr;
         }
-        return (int)prev[n];
+        return (int)dp[n];
     }
 };
