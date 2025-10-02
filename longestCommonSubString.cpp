3b4a0fa19c37b1ecbb52c46a5e029c0c5ea845aa
ABHIJITH G <20309@gtl.loc>
2025-10-02 14:06:07 +0530

    Implemented the longest common subsequence using tabulation in single array for space optimisation

--- longestCommonSubString.cpp
+++ longestCommonSubString.cpp
@@ -3,18 +3,22 @@ class Solution {
     
     int longestCommonSubstr(string& s1, string& s2) {
         vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,0));
+        
         //lengthOfSubStr(s1.size()-1,s2.size()-1,s1,s2,dp);
+        vector<int>prev(s2.size()+1,0);
+        vector<int>curr(s2.size()+1,0);
         int ans = 0;
         for(int i =1;i<=s1.size();i++ ){
             for(int j=1;j<=s2.size();j++){
                 if(s1[i-1]==s2[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
-                    ans = max(ans,dp[i][j]);
+                    curr[j] = 1+prev[j-1];
+                    ans = max(ans,curr[j]);
                     
                 }else
-                   dp[i][j]=0;
+                   curr[j]=0;
                 
             }
+            prev=curr;
         }
         return ans;
         
