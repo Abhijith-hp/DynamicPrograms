a1a24ed6e4b3214e2427850d989548deb4618a86
ABHIJITH G <20309@gtl.loc>
2025-10-02 17:29:10 +0530

    Implemented the program to findout minimum operation to make string using memoziation

--- minOperationsPalindrome.cpp
+++ minOperationsPalindrome.cpp
@@ -14,12 +14,14 @@ public:
         return true;
         
     }
-    int lengthofCommonPalindromeSubsequence(int i,int j,string s1,string s2){
+    int lengthofCommonPalindromeSubsequence(int i,int j,string s1,string s2,vector<vector<int>>&dp){
         if(i<0 || j<0) return 0;
-
+        if(dp[i][j]!=-1) return dp[i][j];
         if(s1[i]==s2[j])
-             return 1+ lengthofCommonPalindromeSubsequence(i-1,j-1,s1,s2);
-        return max(lengthofCommonPalindromeSubsequence(i-1,j,s1,s2),lengthofCommonPalindromeSubsequence(i,j-1,s1,s2));
+            dp[i][j]= 1+ lengthofCommonPalindromeSubsequence(i-1,j-1,s1,s2,dp);
+        else
+            dp[i][j]= max(lengthofCommonPalindromeSubsequence(i-1,j,s1,s2,dp),lengthofCommonPalindromeSubsequence(i,j-1,s1,s2,dp));
+        return dp[i][j];
     }
     int minInsertions(string s) {
         if(isPalindrome(s))
@@ -27,7 +29,8 @@ public:
         int size = s.size();
         string s1= s;
         reverse(s.begin(),s.end());
-        return size - lengthofCommonPalindromeSubsequence(size-1,size-1,s1,s);
+        vector<vector<int>>dp(size,vector<int>(size,-1));
+        return size - lengthofCommonPalindromeSubsequence(size-1,size-1,s1,s,dp);
         
 
     }
