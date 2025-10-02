1cc7da3a93fdf63fa13c4ddd1bdcffb047159778
ABHIJITH G <20309@gtl.loc>
2025-10-02 16:49:35 +0530

    Implemented the longest common palindrome  subsequence using tabulation in single array for space optimization

--- longestCommonPalindromeSubsequence.cpp
+++ longestCommonPalindromeSubsequence.cpp
@@ -4,17 +4,19 @@ public:
         int length = s.size();
         string s1 = s;
         reverse(s.begin(),s.end());
-        vector<vector<int>>dp(length+1,vector<int>(length+1,0));
+        vector<int>prev(length+1,0);
+        vector<int>curr(length+1,0);
 
         for(int i=1;i<=length;i++){
             for(int j=1;j<=length;j++){
                 if(s1[i-1]==s[j-1])
-                    dp[i][j] = 1+ dp[i-1][j-1];
+                    curr[j] = 1+ prev[j-1];
                 else
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                    curr[j] = max(prev[j],curr[j-1]);
             }
+            prev=curr;
         }
-        return dp[length][length];
+        return prev[length];
         
     }
 };
