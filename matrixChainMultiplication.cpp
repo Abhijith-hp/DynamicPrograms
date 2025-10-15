0c7422a6720d3b531e08bee15db1f07c248682e5
ABHIJITH G <20309@gtl.loc>
2025-10-15 10:58:58 +0530

    Implemented matrix chain multiplication using memoziation

--- matrixChainMultiplication.cpp
+++ matrixChainMultiplication.cpp
@@ -1,21 +1,22 @@
 class Solution {
   public:
-    int helper(int i,int j,vector<int>&arr){
+    int helper(int i,int j,vector<int>&arr,vector<vector<int>>&dp){
         if(i==j) return 0;
         int mini = INT_MAX;
-        
+        if(dp[i][j]!=-1) return dp[i][j];
         for(int k=i;k<j;k++){
-            int steps = (arr[i-1] * arr[k] * arr[j]) + helper(i,k,arr)+helper(k+1,j,arr);
+            int steps = (arr[i-1] * arr[k] * arr[j]) + helper(i,k,arr,dp)+helper(k+1,j,arr,dp);
             mini  = min(steps,mini);
         }
        
-        return mini;
+        return dp[i][j]=mini;
         
         
     }
     int matrixMultiplication(vector<int> &arr) {
-      
-      return helper(1,arr.size()-1,arr);
+      int n = arr.size();
+      vector<vector<int>>dp(n,vector<int>(n,-1));
+      return helper(1,n-1,arr,dp);
         
     }
 };
