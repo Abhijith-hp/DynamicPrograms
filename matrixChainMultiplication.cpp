c2b37c835177056bdb42ade6db182e254ff17849
ABHIJITH G <20309@gtl.loc>
2025-10-15 11:00:17 +0530

    Implemented matrix chain multiplication using tabulation

--- matrixChainMultiplication.cpp
+++ matrixChainMultiplication.cpp
@@ -1,22 +1,21 @@
 class Solution {
   public:
-    int helper(int i,int j,vector<int>&arr,vector<vector<int>>&dp){
-        if(i==j) return 0;
-        int mini = INT_MAX;
-        if(dp[i][j]!=-1) return dp[i][j];
-        for(int k=i;k<j;k++){
-            int steps = (arr[i-1] * arr[k] * arr[j]) + helper(i,k,arr,dp)+helper(k+1,j,arr,dp);
-            mini  = min(steps,mini);
-        }
-       
-        return dp[i][j]=mini;
-        
-        
-    }
+    
     int matrixMultiplication(vector<int> &arr) {
       int n = arr.size();
+    
       vector<vector<int>>dp(n,vector<int>(n,-1));
-      return helper(1,n-1,arr,dp);
+      for(int i=0;i<n;i++) dp[i][i] = 0;
+      for(int i=n-1;i>=1;i--){
+          for(int j=i+1;j<n;j++){
+              dp[i][j] = INT_MAX;
+              for(int k=i;k<j;k++){
+            int steps = (arr[i-1] * arr[k] * arr[j]) + dp[i][k]+dp[k+1][j];
+            dp[i][j]  = min(steps,dp[i][j]);
+        }
+          }
+      }
+      return dp[1][n-1];
         
     }
 };
