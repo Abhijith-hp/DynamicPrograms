244f7ed2ec781e232a29e265bb6520e886447c3f
ABHIJITH G <20309@gtl.loc>
2025-10-22 16:24:38 +0530

    Implemented palindrome partitioning using tabulation

--- PAlindromePartitioning.cpp
+++ PAlindromePartitioning.cpp
@@ -7,24 +7,21 @@ class Solution {
         }
         return true;
     }
-    int minCuts(int i,string &s,vector<int>&dp){
-       
-        int n = s.size();
-        if(i==n) return 0;
-         if(dp[i]!=-1) return dp[i];
-       
-        int mini = INT_MAX;
+    int palPartition(string &s) {
+       int n = s.size();
+       vector<int>dp(n+1,0);
+       dp[n]=0;
+       for(int i=n-1;i>=0;i--){
+           int mini = INT_MAX;
         for(int j=i;j<n;j++){
             if(isPalindrome(i,j,s)){
-               int cost = 1 + minCuts(j+1,s,dp);
+               int cost = 1 + dp[j+1];
                mini = min(mini,cost);
             }
-        }
-        return dp[i]= mini;
-    }
-    int palPartition(string &s) {
-       vector<int>dp(s.size(),-1);
-       return minCuts(0,s,dp)-1;
+        } 
+        dp[i]=mini;
+       }
+       return dp[0]-1;
         
     }
 };
