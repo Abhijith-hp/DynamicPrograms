c75ef503184ea75a4a128adf3c9349bfcb6baecd
ABHIJITH G <20309@gtl.loc>
2025-10-22 17:50:34 +0530

    Implemented partitioning of array using tabulation

--- ArrayPartioning.cpp
+++ ArrayPartioning.cpp
@@ -1,23 +1,21 @@
 class Solution {
   public:
-    int helper(int index,int k,vector<int>&arr,vector<int>&dp){
-        int n = arr.size();
-        if(index==n) return 0;
-        int len = 0; 
-        int maxi = INT_MIN, maxOfSum = INT_MIN;
-        if(dp[index]!=-1) return dp[index];
-        for(int i=index;i<min(index+k,n);i++){
+ 
+    int solve(int n, int k, vector<int>& arr) {
+        vector<int>dp(n+1,0);
+        for(int index=n-1;index>=0;index--){
+           int len = 0; 
+           int maxi = INT_MIN, maxOfSum = INT_MIN;
+           for(int i=index;i<min(index+k,n);i++){
            len++;
            maxi = max(maxi,arr[i]);
-           int sum= maxi*len + helper(i+1,k,arr,dp);
+           int sum= maxi*len + dp[i+1];
            maxOfSum = max(sum,maxOfSum);
            
         }
-        return dp[index] =  maxOfSum;
-    }
-    int solve(int n, int k, vector<int>& arr) {
-        vector<int>dp(n,-1);
-        return helper(0,k,arr,dp);
+        dp[index]=maxOfSum;
+        }
+        return dp[0];
         
     }
-}
+};
