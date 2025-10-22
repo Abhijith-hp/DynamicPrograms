58364d929a5161822a8802a0346fed9ef3c82f9d
ABHIJITH G <20309@gtl.loc>
2025-10-22 17:44:11 +0530

    Implemented partitioning of array using memosiation

--- ArrayPartioning.cpp
+++ ArrayPartioning.cpp
@@ -1,22 +1,23 @@
 class Solution {
   public:
-    int helper(int index,int k,vector<int>&arr){
+    int helper(int index,int k,vector<int>&arr,vector<int>&dp){
         int n = arr.size();
         if(index==n) return 0;
         int len = 0; 
         int maxi = INT_MIN, maxOfSum = INT_MIN;
-        
+        if(dp[index]!=-1) return dp[index];
         for(int i=index;i<min(index+k,n);i++){
            len++;
            maxi = max(maxi,arr[i]);
-           int sum= maxi*len + helper(i+1,k,arr);
+           int sum= maxi*len + helper(i+1,k,arr,dp);
            maxOfSum = max(sum,maxOfSum);
            
         }
-        return maxOfSum;
+        return dp[index] =  maxOfSum;
     }
     int solve(int n, int k, vector<int>& arr) {
-        return helper(0,k,arr);
+        vector<int>dp(n,-1);
+        return helper(0,k,arr,dp);
         
     }
-};
+}
